28d7ede02a27fd94fd1ebaac9072e25d7678204c
averagedude05 <adibkhan642@gmail.com>
2026-05-25 13:09:49 +0600

    minor changes

--- Web Scrapper Using Python._Real_Websitpy.py
+++ Web Scrapper Using Python._Real_Websitpy.py
@@ -8,15 +8,13 @@ notices=soup.find_all("h2", class_="title")
 #General Syntax of a genearator statement
 # (expression for item in iterable if condition) 
 def check_keyword(title_text) :
-   words_intitle=title_text.split() 
-   has_exam_routine= "exam" in title_text and "routine" in title_text
+  
+   has_exam_routine= "exam" in title_text and "schedule" in title_text
    if has_exam_routine:
        exam_type_and_dept= "mid" in title_text or "final" in title_text and "fst" in title_text
-       
        return exam_type_and_dept
 for n in notices:
     title_text = n.text.strip().lower()
-   
     if check_keyword(title_text):
         print(title_text)
       
