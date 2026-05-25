64781831a07409088667edeab7f231e78cccae42
averagedude05 <adibkhan642@gmail.com>
2026-05-25 11:57:36 +0600

    show notices with matching keywords

--- Web Scrapper Using Python._Real_Websitpy.py
+++ Web Scrapper Using Python._Real_Websitpy.py
@@ -1,4 +1,30 @@
-import requests #requests info from a website
+from bs4 import BeautifulSoup
+import requests
 
-html_text=requests.get('https://bdjobs.com/h/jobs?qOT=&txtsearch=python&lang=en').text
-print(html_text)
+html_text=requests.get('https://www.aiub.edu/category/notices').text
+soup=BeautifulSoup(html_text, 'lxml')
+notices=soup.find_all("h2", class_="title")
+keywords_allowed={"exam",
+    "mid",
+    "final",
+    "fst"}
+keywords_notallowed={"seat plan",
+    "set b"}
+#General Syntax of a genearator statement
+# (expression for item in iterable if condition) 
+def check_keyword(title_text) :
+   words_intitle=title_text.split() 
+   has_exam_routine= "exam" in title_text and "routine" in title_text
+   if has_exam_routine:
+       exam_type_and_dept= "mid" in title_text or "final" in title_text and "fst" in title_text
+       
+       return exam_type_and_dept
+for n in notices:
+    title_text = n.text.strip().lower()
+    # allowed=any(words in title_text.lower() for words in keywords_allowed)
+    # forbidden_word=any(words in title_text.lower() for words in keywords_notallowed)
+    if check_keyword(title_text):
+        print(title_text)
+      
+ 
+
