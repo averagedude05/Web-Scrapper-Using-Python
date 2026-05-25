1cce44a97c43a1f07e03f4093b4fbabbf7e852ad
averagedude05 <adibkhan642@gmail.com>
2026-05-25 12:05:07 +0600

    minor changes

--- Web Scrapper Using Python._Real_Websitpy.py
+++ Web Scrapper Using Python._Real_Websitpy.py
@@ -4,12 +4,7 @@ import requests
 html_text=requests.get('https://www.aiub.edu/category/notices').text
 soup=BeautifulSoup(html_text, 'lxml')
 notices=soup.find_all("h2", class_="title")
-keywords_allowed={"exam",
-    "mid",
-    "final",
-    "fst"}
-keywords_notallowed={"seat plan",
-    "set b"}
+
 #General Syntax of a genearator statement
 # (expression for item in iterable if condition) 
 def check_keyword(title_text) :
@@ -21,8 +16,7 @@ def check_keyword(title_text) :
        return exam_type_and_dept
 for n in notices:
     title_text = n.text.strip().lower()
-    # allowed=any(words in title_text.lower() for words in keywords_allowed)
-    # forbidden_word=any(words in title_text.lower() for words in keywords_notallowed)
+   
     if check_keyword(title_text):
         print(title_text)
       
