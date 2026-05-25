2d11bfd8c5f8c2a242c247bdae12b471c0e6b3dc
averagedude05 <adibkhan642@gmail.com>
2026-05-25 16:23:16 +0600

    setup of telegram bot and changes to gitignore

--- Web Scrapper Using Python._Real_Websitpy.py
+++ Web Scrapper Using Python._Real_Websitpy.py
@@ -1,22 +1,41 @@
+import os
+import time
 from bs4 import BeautifulSoup
 import requests
+import typing
+from dotenv import load_dotenv
 
-html_text=requests.get('https://www.aiub.edu/category/notices').text
+
+#telegram Setup
+load_dotenv()
+token: typing.Final=os.getenv("TELEGRAM_TOKEN") #show a warning by text editor or ide saying can't assign another value to the variable
+chat_id: typing.Final=os.getenv("MY_CHAT_ID")
+username: typing.Final=os.getenv("USERNAME")
+
+#Webscrapper setup
+html_text=requests.get('https://www.aiub.edu/category/notices?pageNo=4&pageSize=20').text
 soup=BeautifulSoup(html_text, 'lxml')
-notices=soup.find_all("h2", class_="title")
+seen_notices=set()
+
+
 
 #General Syntax of a genearator statement
 # (expression for item in iterable if condition) 
 def check_keyword(title_text) :
-  
-   has_exam_routine= "exam" in title_text and "schedule" in title_text
-   if has_exam_routine:
-       exam_type_and_dept= "mid" in title_text or "final" in title_text and "fst" in title_text
-       return exam_type_and_dept
-for n in notices:
-    title_text = n.text.strip().lower()
-    if check_keyword(title_text):
-        print(title_text)
-      
- 
+    return "exam" in title_text and "schedule" in title_text
+def find_exam():
+    for n in notices:
+        title_text = n.text.strip().lower()
+        if check_keyword(title_text):
+            if(title_text not in seen_notices):
+                print(title_text)
+                seen_notices.add(title_text)
+    
+if __name__=="__main__":
+    while True:
+        notices=soup.find_all("h2", class_="title")
+        find_exam()
+        time_wait=15
+        print("Waiting for: ",time_wait," minutes")
+        time.sleep(time_wait*60)
 
